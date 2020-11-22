297eddcd1ee3855eb1f069da4c4d1ba403bdccc0
Max Schwarz <max.schwarz@online.de>
2020-11-22 16:04:29 +0100

    add new full.h header (see #2)

---
+++ include/rosfmt/full.h
@@ -0,0 +1,11 @@
+// rosfmt - type-safe ROS_* logging macros: Full version
+// Author: Max Schwarz <max.schwarz@ais.uni-bonn.de>
+
+#ifndef ROSFMT_FULL_H
+#define ROSFMT_FULL_H
+
+#include <rosfmt/rosfmt.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+
+#endif
